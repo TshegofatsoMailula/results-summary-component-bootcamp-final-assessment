bfd05608f8217b30f4dff6a9eda24fb4e6d28910
Tshegofatso Mailula <tshego.mailula@gmail.com>
2026-06-08 21:04:46 +0200

    Calculate average

--- assets/js/async.js
+++ assets/js/async.js
@@ -7,6 +7,7 @@ async function display_results()
     const fetch_results = await fetch("./data.json");
     const results = await fetch_results.json();
     const results_div = document.getElementById("results");
+    let average = 0;
     results.forEach((result)=>{
         const item = document.createElement("div");
         item.classList.add("category-card");
@@ -17,5 +18,9 @@ async function display_results()
             <label class='score'><span>${result.score}</span> / 100</label>
         `;
         results_div.appendChild(item);
+        average = average + result.score;
     });
+    average = average / results.length;
+    average = Math.round(average);
+    document.getElementById("average").innerHTML = average;
 }
