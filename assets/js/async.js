bf2b31057efbb379581bbc8ad1ad9257c368fd54
Tshegofatso Mailula <tshego.mailula@gmail.com>
2026-06-08 20:55:48 +0200

    Display results from data.json file

--- assets/js/async.js
+++ assets/js/async.js
@@ -6,5 +6,16 @@ async function display_results()
 {
     const fetch_results = await fetch("./data.json");
     const results = await fetch_results.json();
-    console.log(results);
+    const results_div = document.getElementById("results");
+    results.forEach((result)=>{
+        const item = document.createElement("div");
+        item.classList.add("category-card");
+        item.style.backgroundColor = result.background;
+        item.style.color = result.text;
+        item.innerHTML = `
+            <label class='category'><img src='${result.icon}' alt='${result.category} icon'></img>${result.category}</label>
+            <label class='score'><span>${result.score}</span> / 100</label>
+        `;
+        results_div.appendChild(item);
+    });
 }
